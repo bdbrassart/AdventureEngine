d39bf971b184b6b9a22b17b564efcfd41402ffd4
Ben Brassart <ben.brassart@gmail.com>
2023-06-18 10:07:10 -0500

    added locations file and movement functions

--- advEng.py
+++ advEng.py
@@ -1,5 +1,5 @@
 
-
+import json
 # Collection of classes for "Adventure Engine"
 # please use camelcase
 
@@ -14,6 +14,7 @@ class mainChar:
         self.race = race
         self.hpTotal = 20
         self.hpCurr = 20
+        self.locID = "locID_1"
     
     def print(self):
         print("Name: " + self.name)
@@ -23,15 +24,29 @@ class mainChar:
 class advEngEnv:
     player: mainChar
     commands: dict
+    userInput: str
 
-    def __init__(self, player):
+    def __init__(self, player, locations):
+        # Load the player
         self.player = player
+        
+        # Load the locations files
+        with open(locations, 'r') as file:
+            self.locations = json.load(file)
+
+        # Set aliases for commands
         self.commands = {
             "help": self.showHelp,
             "look": self.playerLook,
-            "sit": self.playerSit
+            "sit": self.playerSit,
+            "n": self.playerMove,
+            "s": self.playerMove,
+            "e": self.playerMove,
+            "w": self.playerMove
         }
-        
+
+    ### THIS IS THE COMMAND SECTION
+    
     def showHelp(self):
         self.name = "help"
         self.helpFile = "Lists all available commands."
@@ -40,12 +55,35 @@ class advEngEnv:
     def playerLook(self):
         self.name = "look"
         self.helpfile = "Look around the area."
-        print("This is the look command")
+        
+        # Pull info from locations data
+        locTitle = self.locations[self.player.locID][0]['locTitle']
+        locDesc = self.locations[self.player.locID][0]['locDesc']
+        locExits = self.locations[self.player.locID][0]['locExits']
+        locExitsStr = ', '.join(key for item in locExits for key in item.keys())
+        # Format things prettily
+        print("\n")
+        print("\033[1m" + locTitle + "\033[0m")
+        print(locDesc)
+        print("\033[1mExits: \033[0m" + locExitsStr)
+        print("\n")
+        
 
     def playerSit(self):
         self.name = "sit"
         self.helpfile = "Sit down."
         print("This is the sit command")
 
+    def playerMove(self):
+        # Calculate the adjacent locations from locations data
+        adjLocs = self.locations[self.player.locID][0]['locExits']
 
+        # Determine the destination locID from the input direction
+        destLoc = adjLocs[0][self.userInput]
+      
+        # Set the players new location
+        self.player.locID = destLoc
+    
+        # Look around
+        self.playerLook()
 
--- main.py
+++ main.py
@@ -5,16 +5,22 @@ def main():
     #load player
     player = mainChar("Bedail", "Human")
 
+    #load locations
+    locations_file = "locations.json"
+
     #create environment
-    worldEnv = advEngEnv(player)
+    worldEnv = advEngEnv(player, locations_file)
     
     # input loop
+    
+    worldEnv.playerLook()
 
     while True:
         userInput = input(player.name + " || " + str(player.hpCurr) + "//" + str(player.hpTotal) + " HP> ")
-        
+
         # This runs the command functions if it matches
         if userInput in worldEnv.commands:
+            worldEnv.userInput = userInput
             command = worldEnv.commands[userInput]
             command()
         elif userInput.lower() == "exit":
