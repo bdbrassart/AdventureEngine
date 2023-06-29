ad7321e08d30f80eda72daab2f3ebb1410ac0f81
Ben Brassart <ben.brassart@gmail.com>
2023-06-29 07:28:59 -0500

    brought threading into the main env class

--- npcActions.py
+++ npcActions.py
@@ -1,12 +1,1 @@
-def npcID_1(npcObj, window, event, stopEvent):
-    # Reginald Kensington
-    while not stopEvent.is_set():
-        window.addstr("test npc 1\n\n")
-        event.wait(5)
-        window.refresh()
-
-def npcID_2(npcObj, window, event, stopEvent):
-    while not stopEvent.is_set():
-        window.addstr("test npc 2\n\n")
-        event.wait(3)
-        window.refresh()
+import curses
